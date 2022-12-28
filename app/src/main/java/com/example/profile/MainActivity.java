f54a9470c476cf124c2f6ae6c42a7b88a89cfc01
jianqi02 <jianqi02@gmail.com>
2022-12-28 19:24:54 +0800

    Change

---
+++ app/src/main/java/com/example/profile/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.profile;
+
+import static com.example.profile.R.menu.profile_menu;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_profile_page_1);
+
+
+    }
+}
